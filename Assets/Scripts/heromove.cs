5189a989ae6aa19c4fa6b208ee2371139fe86a24
Alk0rt <allbor542@gmail.com>
2025-11-09 10:49:23 +0200

    add basic movement

---
+++ Assets/Scripts/heromove.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class heromove : MonoBehaviour
+{
+    [Header("Move")]
+    public float moveSpeed = 7f;
+
+    [Header("Jump")]
+    public float jumpForce = 12f;
+    public float fallMultiplier = 2.2f;      
+    public float lowJumpMultiplier = 3f;     
+
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+       
+        float move = Input.GetAxisRaw("Horizontal"); 
+        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+
+      
+        if (move != 0)
+            transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+     
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+    }
+}
