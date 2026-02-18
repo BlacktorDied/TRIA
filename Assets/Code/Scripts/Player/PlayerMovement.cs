04b409e00994b6ddbe391d44b1307d4fb36f7d88
Alexander Dozorin <alex.dozorin@gmail.com>
2026-02-18 15:15:57 +0200

    [HP-45] After Rebase Fixup

--- Assets/Code/Scripts/Player/PlayerMovement.cs
+++ Assets/Code/Scripts/Player/PlayerMovement.cs
@@ -1,12 +1,9 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-
 public class PlayerMovement : MonoBehaviour
 {
     public bool IsGrounded;
-
-    #region Variables
 
     [Header("Movement Settings")]
     [SerializeField] private float walkSpeed = 5f;
@@ -22,10 +19,6 @@ public class PlayerMovement : MonoBehaviour
     private PlayerAudio playerAudio;
     private PlayerDash dash;
     private Animator anim;
-
-    #endregion
-
-    #region Unity Methods
 
     private void Awake()
     {
@@ -47,42 +40,36 @@ public class PlayerMovement : MonoBehaviour
             groundLayer
         ).collider != null;
     }
+
     private void FixedUpdate()
     {
-        PlayerDash dash = GetComponent<PlayerDash>();
         if (dash != null && dash.IsDashing)
         {
             playerAudio?.StopFootsteps();
+            anim?.SetBool("isWalking", false);
             return;
         }
 
         float x = input.MoveInput.x;
+
+        // Move
         rb.linearVelocity = new Vector2(x * walkSpeed, rb.linearVelocity.y);
 
+        // Anim
+        anim?.SetBool("isWalking", Mathf.Abs(x) > 0.1f && IsGrounded);
+
+        // Footsteps
         HandleFootsteps(x);
+
+        // Facing direction
         Flip(x);
     }
 
-    #endregion
-
-    private void HandleFootsteps(float xInput) {
-        if (dash != null && dash.IsDashing) return;
-
-        Move();
-    }
-
-    void Move()
+    private void HandleFootsteps(float xInput)
     {
-        rb.linearVelocity = new Vector2(input.MoveInput.x * walkSpeed, rb.linearVelocity.y);
-        Flip();
-        anim.SetBool("isWalking", rb.linearVelocity.x != 0 && IsGrounded);
-    }
-
-    void Flip()
-    {
-        bool isWalking = Mathf.Abs(xInput) > 0.1f && IsGrounded;
-
         if (playerAudio == null) return;
+
+        bool isWalking = Mathf.Abs(xInput) > 0.1f && IsGrounded;
 
         if (isWalking) playerAudio.StartFootsteps();
         else playerAudio.StopFootsteps();
@@ -90,9 +77,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void Flip(float xInput)
     {
-        if (xInput > 0)
+        if (xInput > 0f)
             transform.localScale = new Vector3(1f, 0.875f, 1f);
-        else if (xInput < 0)
+        else if (xInput < 0f)
             transform.localScale = new Vector3(-1f, 0.875f, 1f);
     }
 
